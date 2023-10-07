c81fdcc5caed0acb3c066adfc22e789d56c79357
quanghuy-hoang <hoangquanghuy.work@gmail.com>
2023-10-07 19:57:31 +0700

    add shadcn preset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,9 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import { shadcnPreset } from "./lib/shadcn/shadcn-preset";
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+const config = {
+  presets: [shadcnPreset],
+  content: ["./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}"],
+} satisfies Config;
+
+export default config;
